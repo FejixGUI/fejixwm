dd7f95affb6696a74f0c3722683a82c0881e0809
MarkLagodych <lgd.mrk@gmail.com>
2022-12-31 18:35:56 +0200

    Finally defined platform api

--- fejixwm_core/src/errors.rs
+++ fejixwm_core/src/errors.rs
@@ -1,5 +1,16 @@
 pub type Result<T> = std::result::Result<T, Error>;
 
-pub enum Error {
 
+pub enum Error {
+    /// Means that a certain feature is not provided or not implemented on the platform
+    Unsupported,
+
+    /// Means that the operating system's graphical environment has behaved in an unexpected way
+    PlatformApiFailed,
+    
+    /// May be caused when a certain graphics API is not supported
+    GraphicsApiFailed,
+
+    /// May be caused by memory allocation failures, mutex creation failures etc.
+    InternalLogicFailed,
 }
